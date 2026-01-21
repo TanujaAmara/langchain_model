8db753cefe06398a177cddf955f9b2480304fc6b
tanuja <amaratanuja1@gmail.com>
2026-01-21 18:06:59 +0530

    make changes regarding to model

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import arxiv
 load_dotenv()
 
 # Initialize LLM
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 parser = StrOutputParser()
 
 
